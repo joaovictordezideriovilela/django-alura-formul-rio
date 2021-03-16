03d9c974d434a8bca6cc37706225f733781c366b
Joao Vilela <joaovictordezideriovilela@gmail.com>
2021-03-16 10:48:34 -0300

    Final curso

--- passagens/forms.py
+++ passagens/forms.py
@@ -3,21 +3,25 @@ from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classe
 from passagens.validation import *
+from passagens.models import Pessoa, Passagem, ClasseViagem
 
-class PassagensForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=200)
-    destino = forms.CharField(label='Destino', max_length=200)
-    data_ida = forms.DateField(label='Data ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Data volta', widget=DatePicker())
+class PassagensForms(forms.ModelForm):
     data_pesquisa = forms.DateField(label='Data pesquisa', disabled=True, initial=datetime.today())
-    classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipos_de_classe)
-    informacoes = forms.CharField(
-        label='Informações extras',
-        max_length=200,
-        widget=forms.Textarea(),
-        required=False
-    )
-    email = forms.EmailField(label='E-mail', max_length=200)
+
+    class Meta:
+        model = Passagem
+        fields = '__all__'
+        labels = {
+            'data_ida': 'Data de ida',
+            'data_volta': 'Data de volta',
+            'classe_viagem': 'Classe da viagem',
+            'informacoes': 'Informações'
+        }
+        widgets = {
+            'data_ida': DatePicker(),
+            'data_volta': DatePicker()
+        }
+
 
     def clean(self):
         origem = self.cleaned_data.get('origem')
@@ -35,3 +39,9 @@ class PassagensForms(forms.Form):
             for erro, mensagem_erro in lista_erros.items():
                 self.add_error(erro, mensagem_erro)
         return self.cleaned_data
+
+class PessoaForms(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        exclude = ['nome']
+        #fields = ['email']
--- passagens/views.py
+++ passagens/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from passagens.forms import PassagensForms
+from passagens.forms import PassagensForms, PessoaForms
 
 # Create your views here.
 def index(request):
-    form = PassagensForms()
-    return render(request, 'index.html', {'form': form})
+    passagem_form = PassagensForms()
+    pessoa_form = PessoaForms()
+    return render(request, 'index.html', {'form': passagem_form,
+                                          'pessoa_form': pessoa_form})
 
 def revisao_consulta(request):
     if request.method == 'POST':
         form = PassagensForms(request.POST)
+        pessoa_form = PessoaForms(request.POST)
         if form.is_valid():
-            return render(request, 'minha_consulta.html', {'form': form})
+            return render(request, 'minha_consulta.html', {'form': form,
+                                                           'pessoa_form': pessoa_form})
         else:
-            return render(request, 'index.html', {'form': form})
+            return render(request, 'index.html', {'form': form,
+                                                  'pessoa_form': pessoa_form})
